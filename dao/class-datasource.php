eac1c71e5daaac0b410ae17d0b1fd9faea729f75
Melissa del Cerro <mdelcerrop@fp.uoc.edu>
2019-04-19 17:48:36 +0200

    Update in datasource to limit number of connections

--- dao/class-datasource.php
+++ dao/class-datasource.php
@@ -1,18 +1,31 @@
 <?php
 
 
-class Datasource
+class datasource
 {
 
+    private static $instance;
     private $connection;
 
-    public function __construct()
+    public static function get_instance()
+    {
+        if (!self::$instance instanceof self) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+
+    private function __construct() {
+
+    }
+
+    private function create_connection()
     {
         $servername = "localhost";
         $username = "root";
         $password = "";
         $schema = "rottenBoardEN";
-        
+
         // Conectar DB
         $this->connection = new mysqli($servername, $username, $password);
         mysqli_set_charset($this->connection, "utf8");
@@ -26,8 +39,20 @@ class Datasource
             die("<br><p style=\"color:red\">La base de datos " . $schema . " no se ha podido seleccionar: </p><br>" . $this->connection->error);
         }
     }
+
     public function get_connection()
     {
+        if ($this->connection == null) {
+            $this->create_connection();
+        }
+
         return $this->connection;
+    }
+
+    public function close_connection() {
+        if ($this->connection != null) {
+            $this->connection->close();
+            $this->connection = null;
+        }
     }
 }
