4bf0b3fd11555c34414347eafcf0cb1a77cd6812
Iris Martínez <iris.martinez.solano@gmail.com>
2019-05-30 20:29:29 +0200

    punctuations Dao y test

--- dao/class-userpunctuategameDao.php
+++ dao/class-userpunctuategameDao.php
@@ -40,21 +40,87 @@ class punctuationsDAO
 
     }
 
-    public function update_punctuations($punctuation)
+    public function get_punctuations_by_game($id_game): array
     {
         $conn = $this->datasource->get_connection();
-        $sql = "UPDATE userpunctuategame SET punctuation = ? WHERE user_id = ?";
+        $sql = "SELECT *
+                FROM UserPunctuateGame
+                WHERE id_game = ?";
+
+        $id = null;
         // Vincular variables a una instrucción preparada como parámetros
         $stmt = $conn->prepare($sql);
+        $stmt->bind_param('d', $id_game);
+        $stmt->execute();
+        $punctuations = $this->extract_result_list($stmt);
+        $stmt->close();
+        return $punctuations;
+    }
+
+    public function get_punctuation_by_id($id): ?Punctuation
+    {
+        $conn = $this->datasource->get_connection();
+        $sql = "SELECT *
+                FROM UserPunctuateGame 
+                WHERE id_punctuation = ?";
+        // Vincular variables a una instrucción preparada como parámetros
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('d', $id);
+        $stmt->execute();
+        $punctuation = $this->extract_single_result($stmt);
+        $stmt->close();
+        return $punctuation;
+    }
+
+    public function update_punctuation($punctuation)
+    {
+        $conn = $this->datasource->get_connection();
+        $sql = "UPDATE UserPunctuatetGame SET punctuation = ? WHERE id_punctuation = ?";
+        $stmt = $conn->prepare($sql);
+
         $id = $punctuation->get_id();
-        $user_id = $punctuation->get_user_id();
-        $game_id = $punctuation->get_game_id();
-        $punct = $punctuation->get_punctuation();
-        $user_level_id = $punctuation->get_user_level_id();
-        $stmt->bind_param('ddddd', $id, $user_id, $game_id, $punct, $user_level_id);
+        $punctuation_name = $punctuation->get_punctuation();
+
+        $stmt->bind_param('sd', $punctuation_name, $id);
         if ($stmt->execute() === FALSE) {
-            throw new Exception("No has podido actualizar la categoría correctamente" . $conn->error);
+            throw new Exception("No has podido actualizar la puntuación ." . $conn->error);
         }
+        $stmt->close();
+    }
+
+    private function extract_result_list($stmt): array
+    {
+        $stmt->bind_result($id, $id_user, $id_game, $punctuation_name, $create_date, $id_user_level);
+        $punctuations = [];
+        while ($stmt->fetch()) {
+            $punctuation = new punctuations();
+            $punctuation->set_id($id);
+            $punctuation->set_punctuation($punctuation_name);
+            $punctuation->set_date($create_date);
+            $punctuation->set_user_id($id_user);
+            $punctuation->set_game_id($id_game);
+            $punctuation->set_user_level_id($id_user_level);
+
+            $punctuations[] = $punctuation;
+        }
+        return $punctuations;
+    }
+
+    private function extract_single_result($stmt): ?punctuation
+    {
+        $stmt->bind_result($id, $user_id, $game_id, $punctuation_name, $date);
+        $punctuation = null;
+        if ($stmt->fetch()) {
+
+            $punctuation = new punctuations();
+            $punctuation->set_id($id);
+            $punctuation->set_user_id($user_id);
+            $punctuation->set_game_id($game_id);
+            $punctuation->set_punctuation($punctuation_name);
+            $punctuation->set_date($date);
+        }
+
+        return $punctuation;
     }
 
     public function insert_punctuations($punctuation)
