1491e053327c4573ac475ae4f6827ef4cc23e036
Melissa del Cerro <mdelcerrop@fp.uoc.edu>
2019-06-08 15:50:19 +0200

    Delete number in games ranking

--- logic/public_index.php
+++ logic/public_index.php
@@ -116,7 +116,7 @@ $juegos = $gameDAO->list_games();
     <div class="container">
         <div class="text-center">
             <h2 class="section-heading text-uppercase">Ranking</h2>
-            <h3 class="section-subheading text-muted">Los 10 juegos mejor valorados por nuestros usuarios</h3>
+            <h3 class="section-subheading text-muted">Los juegos mejor valorados por nuestros usuarios</h3>
         </div>
         <!-- show the 10 best rated games with a loop -->
         <div class="text-center">
